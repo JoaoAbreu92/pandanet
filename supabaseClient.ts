05f6cbbc2d005a7fe56e062fa96f885fe4e0b87c
JoaoAbreu92 <heriquepib@gmail.com>
2026-08-24 17:41:43 +0000

    refactor: Use environment variables for Supabase config

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,11 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zeczodrgytxqesojpuds.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplY3pvZHJneXR4cWVzb2pwdWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NDMxMjMsImV4cCI6MjA3ODAxOTEyM30.QZFvswt6rCFU6LqMBw221G9u9vXCWO4J_Taktj2hlu4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL or Key not defined in environment variables.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
